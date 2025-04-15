7b3f4402504999358c252ca6d515614fc2e7af2f
Antoine Astruc <anastruc@paul-f4Br3s6.clusters.42paris.fr>
2025-04-15 12:28:34 +0200

    ex01

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: anastruc <anastruc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 17:00:39 by anastruc          #+#    #+#             */
-/*   Updated: 2025/04/14 20:30:03 by anastruc         ###   ########.fr       */
+/*   Updated: 2025/04/15 12:20:32 by anastruc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,17 @@
 #include <fstream>
 #include <string>
 
+// Patrsing note : I decided that a month cannot exceed 12, a day is betweem 1 - 31 regardeless yhe month (february).
+// No specification for the value of the year as soon as the format is correct XXXX(digit).
 int main (int argc, char **argv)
 {
+
     std::string dbPath;
     std::string inputPath;
 
     if (argc == 1)
     {
-        std::cout << "Error: could not open file" << std::endl;
+        std::cout << "Error: could not open file." << std::endl;
         return (1);
     }
     
